a8018a5c1bbd0aa20cb4a113c93c6baec84d7f81
orwel009 <orwelvincent1@gmail.com>
2025-11-17 10:58:53 +0530

    signup completed

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Server is running" });
-});
+app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
